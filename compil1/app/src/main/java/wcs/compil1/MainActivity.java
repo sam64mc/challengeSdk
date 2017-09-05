24862d4cddec5543009350cd6557ee1dfcf22525
sam64mc <sam64mc@gmail.com>
2017-09-05 17:56:30 +0200

    ma premiere application

---
+++ compil1/app/src/main/java/wcs/compil1/MainActivity.java
@@ -0,0 +1,17 @@
+package wcs.compil1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast toast = Toast.makeText(this, "Ceci est ma premiere application", Toast.LENGTH_LONG);
+        toast.show();
+    }
+}
